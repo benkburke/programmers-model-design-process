1ea749f5f59df3080fcb8b11c43f8aefb29e59d5
Ben Burke <benkburke@gmail.com>
2022-11-30 15:46:34 -0800

    Initial commit

---
+++ src/Lesson.test.js
@@ -0,0 +1,17 @@
+// import { rolesDataSource } from "./Source";
+
+it("using direct to viewmodel", async () => {
+  // step 1 - hydrating the programmers model
+  // step 2 - hydrating the ViewModel for users display
+  // expect(usersDisplayViewModel).toEqual([
+  //   "The user rob is a Admin and Teacher",
+  //   "The user alex is a Admin",
+  //   "The user simon is a Teacher and Student",
+  //   "The user jane is a Teacher and Student"
+  // ]);
+  // step 3 - hydrating the ViewModel for stats display
+  // expect(statsViewModel).toEqual({
+  //   userCount: 4,
+  //   activeUserPermissionsCount: 7
+  // });
+});
